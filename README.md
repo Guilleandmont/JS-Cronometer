2aacf50dfe8870d0e48b5c85eab1acb2a0766f28
Guillermo Montero <guilleandmont@gmail.com>
2021-02-04 15:13:41 -0400

    added comments and readme

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 Creating a chronometer using JS. Work on progress!
 Don't press the button more than once!
+
+## The Idea 
+
+A chronometer must measure time from the moment it's started. To do this, we use template literals with the typical time format hh:mm:ss and a function *timer()* (in app.js). The function declares the variables that go into the template literals (lines 7-9) and starts a *setInterval()* (line 10). The *setInterval()* function repeats its first parameter every specified amount of time. This amount of time is given by the second parameter in line 30.
+
+Every time *timer()* is executed, the amount of seconds is incremented, and the program checks two things:
+<li>If the number of seconds is a single digit number, a "0" is appended to it to preserve the format.
+<li>If the number of seconds gets to 60, then the number of minutes is incremented, and seconds are set to 0. This action is repeated for minutes and hours.
+
+## Problems
+
+Many. Amongs them:
+
+<li> The most glaring is that there is no way to stop! Will work on this later.
+<li> The variables are constantly juggled between being strings and numbers. This can be made more elegant. The variables must be strings so we can display single digits in a correct way (for example, 01:02:03).
+<li> Too many ifs. There should be no problem since this is just a small chronometer, but for bigger applications loops and ifs should be minimized! 
+
--- app.js
+++ app.js
@@ -6,28 +6,28 @@ let button = document.querySelector(".start-btn");
 function timer() { 
     let seconds = "00";
     let minutes = "00";
-    let hours = "00";
+    let hours = "00"; // Set times as variables, so we can use them in string literals.
     setInterval(() => {
         chronometer.textContent = `${hours}:${minutes}:${seconds}`;
         seconds++;
-        if (parseInt(seconds) < 10){
+        if (parseInt(seconds) < 10){ //Add 0 to the number of seconds when < 10, to preserve the time format.
             seconds = "0" + seconds;
         }
-        if (seconds == 60){
-            minutes++;
+        if (seconds == 60){ //When 60, seconds are reset to 0 and minutes are incremented.
+            minutes++; 
             seconds = "00";
-            if (parseInt(minutes) < 10){
+            if (parseInt(minutes) < 10){ //Add 0 to the number of minutes when < 10.
                 minutes = "0" + minutes;
             }
         }
-        if (minutes == 60){
+        if (minutes == 60){ //When 60, minutes are reset to 0 and hours are incremented.
             hours++;
             minutes = "00";
-            if (parseInt(hours) < 10){
+            if (parseInt(hours) < 10){ //Add 0 to the number of hours when < 10.
                 hours = "0" + hours;
             }
         } 
-        }, 1000); 
+        }, 1000); //setInterval takes milliseconds as its second argument. In short, the function is repeated every 1000 milliseconds (or every second.)
 }
 
 button.addEventListener("click", () => timer());
