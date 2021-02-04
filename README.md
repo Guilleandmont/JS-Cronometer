3ac58e22d6adea189479c9150f81758bdd4753e8
Guillermo Montero <guilleandmont@gmail.com>
2021-02-04 14:46:29 -0400

    Sort of working prototype. No stop button and not very elegant.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JS-Chronometer
+
+Creating a cronometer using JS. Work on progress!
+Don't press the button more than once!
--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 
-//Playing with some ideas
-document.querySelector(".start-btn").addEventListener("click", () => {
-    document.querySelector(".cronometer").textContent = Date.now();
-})
 
+let cronometer = document.querySelector(".cronometer");
+let button = document.querySelector(".start-btn");
+
+function timer() { 
+    let seconds = "00";
+    let minutes = "00";
+    let hours = "00";
+    setInterval(() => {
+        cronometer.textContent = `${hours}:${minutes}:${seconds}`;
+        seconds++;
+        if (parseInt(seconds) < 10){
+            seconds = "0" + seconds;
+        }
+        if (seconds == 60){
+            minutes++;
+            seconds = "00";
+            if (parseInt(minutes) < 10){
+                minutes = "0" + minutes;
+            }
+        }
+        if (minutes == 60){
+            hours++;
+            minutes = "00";
+            if (parseInt(hours) < 10){
+                hours = "0" + hours;
+            }
+        } 
+        }, 1000); 
+}
+
+button.addEventListener("click", () => timer());
+
+//To do
+//Improve the code. Too many if statements!
+//Add a stop function. 
+ 
+ 
