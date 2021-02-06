714995311754e2a10237cbb2c3f9707af6853039
Guillermo Montero <guilleandmont@gmail.com>
2021-02-06 13:37:32 -0400

    added clear button

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-
 'use strict';
 
 let chronometer = document.querySelector(".chronometer");
-let button = document.querySelector(".start-btn");
+let startButton = document.querySelector("#start-btn");
+let clearButton = document.querySelector("#clear-btn");
 let [h, m, s] = [0,0,0];
 let timerInterval;
 
-function timer() { 
-    button.onclick = () => stop();
-    button.value = "Stop!";
-    //remove onclick timer function
+function start() { 
+    startButton.onclick = () => stop();
+    startButton.value = "Stop!";
+    //remove onclick start function
     timerInterval = setInterval(() => {
         chronometer.textContent = `${h}:0${m}:0${s}`.replace(/\d(?=\d{2})/g, ""); //.replace takes a regex that matches a number (\d, in our case 0) only if that number is followed by two numbers ((?=\d{2})). This way, if the string literal gets a three digit number, the 0 is replaced by an empty string, maintaining the format. 
         s++;
@@ -21,17 +21,26 @@ function timer() {
                 h++;
             }
         }
-    }, 1000)
+    }, 1000);
 }
 
 function stop() {
-    button.onclick = () => timer();
-    button.value = "Continue!";
+    startButton.onclick = () => start();
+    startButton.value = "Continue!";
     console.log("this works");
     clearInterval(timerInterval);
 }
 
-button.onclick = () => timer();
+function clear() {
+    startButton.onclick = () => start();
+    startButton.value = "Start!";
+    clearInterval(timerInterval);
+    [h,m,s] = [0,0,0];
+    chronometer.textContent = `${h}:0${m}:0${s}`;
+}
+
+startButton.onclick = () => start();
+clearButton.onclick = () => clear();
 
 //To do
 //Improve the code. Idea: organize everything into objects and turn functions into methods.
