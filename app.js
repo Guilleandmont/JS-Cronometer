2b1303daba7c3a1a0a5a66b22ccc83ae7f5416da
Guillermo Montero <guilleandmont@gmail.com>
2021-02-27 20:51:27 -0400

    changed readme a bit and reformatted code with Prettier

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-'use strict';
+"use strict";
 
 let chronometer = document.querySelector(".chronometer");
 let startButton = document.querySelector("#start-btn");
 let clearButton = document.querySelector("#clear-btn");
-let [h, m, s] = [0,0,0];
+let [h, m, s] = [0, 0, 0];
 let timerInterval;
 
-function start() { 
+function start() {
     startButton.onclick = () => stop();
     startButton.value = "Stop!";
     //remove onclick start function
     timerInterval = setInterval(() => {
-        chronometer.textContent = `${h}:0${m}:0${s}`.replace(/\d(?=\d{2})/g, ""); //.replace takes a regex that matches a number (\d, in our case 0) only if that number is followed by two numbers ((?=\d{2})). This way, if the string literal gets a three digit number, the 0 is replaced by an empty string, maintaining the format. 
+        chronometer.textContent = `${h}:0${m}:0${s}`.replace(
+            /\d(?=\d{2})/g,
+            ""
+        ); //.replace takes a regex that matches a number (\d, in our case 0) only if that number is followed by two numbers ((?=\d{2})). This way, if the string literal gets a three digit number, the 0 is replaced by an empty string, maintaining the format.
         s++;
         if (s > 59) {
             s = 0;
@@ -35,7 +38,7 @@ function clear() {
     startButton.onclick = () => start();
     startButton.value = "Start!";
     clearInterval(timerInterval);
-    [h,m,s] = [0,0,0];
+    [h, m, s] = [0, 0, 0];
     chronometer.textContent = `${h}:0${m}:0${s}`;
 }
 
